fc01002a3ada66584ceb7ec7e30a5f6cb59640fc
Daniel T. Rodrigues <45438149+danitrod@users.noreply.github.com>
2021-03-01 00:59:35 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ set -eou pipefail
  while IFS=',' read -ra MIGRATION_DIRECTORIES; do
       for directory in "${MIGRATION_DIRECTORIES[@]}"; do
           pushd $directory
+          diesel migration run --database-url $2
           if [[ $SQLITE = true ]] ; then
               sudo chown -R $(whoami) $2
           fi
-          diesel migration run --database-url $2
           popd
       done
  done <<< "$1"

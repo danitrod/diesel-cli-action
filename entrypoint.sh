30442116818b31418acb5bf54817f194d559ceba
Daniel T. Rodrigues <45438149+danitrod@users.noreply.github.com>
2021-03-01 00:35:35 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ set -eou pipefail
  while IFS=',' read -ra MIGRATION_DIRECTORIES; do
       for directory in "${MIGRATION_DIRECTORIES[@]}"; do
           pushd $directory
+          sudo chown -R $(whoami) $directory
           diesel migration run --database-url $2
           popd
       done
